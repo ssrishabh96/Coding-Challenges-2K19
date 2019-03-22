ea61c61bb79d9eabcedc9ca8937e6b5b9fc58915
Rishabh Agrawal <ssrishabh96@users.noreply.github.com>
2019-03-22 11:45:47 -0500

    Correct the corner case condition

--- 1. Reverse_Integer.java
+++ 1. Reverse_Integer.java
@@ -1,26 +1,23 @@
 class Solution {
     public int reverse(int x) {
-        int max=Integer.MAX_VALUE-1;
+        int max=Integer.MAX_VALUE;
         // Use negative flag to flag x as negative and then make it positive
         boolean negative=false;
-        
-        if(x<0 && x<((max+1)*-1)) {
-            return 0;
-        }else  if(x>max) {
-            return 0;
-        }
-        
+               
         // Make the number positive and set the flag
         if(x<0){
             negative=true;
             x=x*(-1);
         }
         
-        int newNum=0;
+        int unit=0, newNum=0;
         while(x>0){
-            newNum=(newNum*10)+(x%10);
+            unit=x%10;
+            // Boundary Conditions
+            if(negative && (newNum==max/10 && unit>7) ) return 0;
+            if((newNum>max/10) || (newNum==max/10 && unit>8) ) return 0;
+            newNum=(newNum*10)+unit;
             x/=10;
-            // Check boundary condition for minimum and maximum unsigned int value 
         }
 
         if(negative){
